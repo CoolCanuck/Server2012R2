8c50283f8ce1323b0b0f91b9e4808ecba108febb
Brayden <CoolCanuck@users.noreply.github.com>
2017-05-13 14:14:34 -0400

    Create COMP2102.ps1
    Uninstalls junk that instructor thought was helpful but is bad practice!

---
+++ COMP2102.ps1
@@ -0,0 +1,4 @@
+wmic product where name="Google Chrome" call uninstall
+wmic product where name="Yahoo" call uninstall
+wmic product where name="Google Update Helper" call uninstall
+wmic product where name="Java" call uninstall
